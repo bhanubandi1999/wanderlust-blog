f25450a3e4c36292f029624f7edac8d8e6a05bc9
Bhanu Bandi <bhanubandi@MacBookPro.lan>
2025-03-11 03:08:08 +0530

    terraform update

--- main.tf
+++ main.tf
@@ -2,36 +2,70 @@ provider "aws" {
   region = var.aws_region  # Use the variable for the region
 }
 
-# ECR Repository
-resource "aws_ecr_repository" "my_repository" {
-  name = var.repository_name
+# ECR Repository for Frontend
+resource "aws_ecr_repository" "frontend_repository" {
+  name = var.frontend_repository_name
 
   force_delete = true
 }
 
-output "repository_url" {
-  value = aws_ecr_repository.my_repository.repository_url
+# ECR Repository for Backend
+resource "aws_ecr_repository" "backend_repository" {
+  name = var.backend_repository_name
+
+  force_delete = true
 }
 
-# Docker image build and push automation using null_resource
-resource "null_resource" "build_and_push_docker_image" {
-  depends_on = [aws_ecr_repository.my_repository]
+output "frontend_repository_url" {
+  value = aws_ecr_repository.frontend_repository.repository_url
+}
+
+output "backend_repository_url" {
+  value = aws_ecr_repository.backend_repository.repository_url
+}
+
+# Docker image build and push automation using null_resource (Frontend)
+resource "null_resource" "build_and_push_frontend_docker_image" {
+  depends_on = [aws_ecr_repository.frontend_repository]
 
   provisioner "local-exec" {
     command = <<EOT
     
       # Login to AWS ECR using AWS CLI
-      $(aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${aws_ecr_repository.my_repository.repository_url})
+      $(aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${aws_ecr_repository.frontend_repository.repository_url})
 
-      # Build the Docker image
-      docker build -t ${var.image_name} -f ./hello-world/Dockerfile .  #Define the path to your Dockerfile
+      # Build the Docker image for frontend
+      cd ./frontend/
+      docker build -t ${var.frontend_image_name} -f Dockerfile .  # Path to your frontend Dockerfile
 
-      # Tag the image with the ECR repository URL
-      docker tag ${var.image_name}:latest ${aws_ecr_repository.my_repository.repository_url}:${var.image_name}-latest
+      # Tag the image with the ECR repository URL for frontend
+      docker tag ${var.frontend_image_name}:latest ${aws_ecr_repository.frontend_repository.repository_url}:${var.frontend_image_name}-latest
 
-      # Push the Docker image to the ECR repository
-      docker push ${aws_ecr_repository.my_repository.repository_url}:${var.image_name}-latest
+      # Push the frontend Docker image to the ECR repository
+      docker push ${aws_ecr_repository.frontend_repository.repository_url}:${var.frontend_image_name}-latest
     EOT
   }
 }
 
+# Docker image build and push automation using null_resource (Backend)
+resource "null_resource" "build_and_push_backend_docker_image" {
+  depends_on = [aws_ecr_repository.backend_repository]
+
+  provisioner "local-exec" {
+    command = <<EOT
+    
+      # Login to AWS ECR using AWS CLI
+      $(aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${aws_ecr_repository.backend_repository.repository_url})
+
+      # Build the Docker image for backend
+      cd  ./backend/
+      docker build -t ${var.backend_image_name} -f Dockerfile .  # Path to your backend Dockerfile
+
+      # Tag the image with the ECR repository URL for backend
+      docker tag ${var.backend_image_name}:latest ${aws_ecr_repository.backend_repository.repository_url}:${var.backend_image_name}-latest
+
+      # Push the backend Docker image to the ECR repository
+      docker push ${aws_ecr_repository.backend_repository.repository_url}:${var.backend_image_name}-latest
+    EOT
+  }
+}
--- vars.tf
+++ vars.tf
@@ -3,12 +3,26 @@ variable "aws_region" {
   default     = "ap-south-1"  # You can modify this to your desired region
 }
 
-variable "repository_name" {
-  description = "Name of the Docker ECR repository"
-  default     = "team-docker-repository"
+# Frontend Repository
+variable "frontend_repository_name" {
+  description = "Name of the frontend Docker ECR repository"
+  default     = "frontend-docker-repository"  # You can modify this to your desired repository name
 }
 
-variable "image_name" {
-  description = "The name of the Docker image"
-  default     = "team-docker-image"  # You can modify this to your desired image name
+# Backend Repository
+variable "backend_repository_name" {
+  description = "Name of the backend Docker ECR repository"
+  default     = "backend-docker-repository"  # You can modify this to your desired repository name
+}
+
+# Frontend Image
+variable "frontend_image_name" {
+  description = "The name of the frontend Docker image"
+  default     = "frontend-docker-image"  # You can modify this to your desired image name
+}
+
+# Backend Image
+variable "backend_image_name" {
+  description = "The name of the backend Docker image"
+  default     = "backend-docker-image"  # You can modify this to your desired image name
 }
